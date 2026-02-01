5bde0c0bdd4a335b78177e61f3a884145539b55f
danielcervera964-lab <danielcervera964@gmail.com>
2026-02-01 17:12:04 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://akoxjqajdetrdvfxdcjv.supabase.co'
-const supabaseKey = 'sb_publicable_a-1IsmFGHxTdkB28fy5ZKw_DHWNiimk'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrb3hqcWFqZGV0cmR2ZnhkY2p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDY4MTAsImV4cCI6MjA4NTUyMjgxMH0.rQVLBEMi76Kfiw3uTfCDWZSa-vxk3s0ALOkkybRMS1I'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
