b5c35efcabf1d74240a2a2fd6b91e91fc1060bd0
danielcervera964-lab <danielcervera964@gmail.com>
2026-02-01 16:47:24 +0100

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = https://akoxjqajdetrdvfxdcjv.supabase.co
+const supabaseKey = sb_publicable_a-1IsmFGHxTdkB28fy5ZKw_DHWNiimk
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
